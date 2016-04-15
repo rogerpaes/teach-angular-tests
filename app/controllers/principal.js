c676ba3388defe03b9be6feef2a232289b7a7bbe
Roger Paes <rogerpaes@gmail.com>
2016-04-15 10:09:00 -0300

    commit4

--- app/controllers/principal.js
+++ app/controllers/principal.js
@@ -1,3 +1,4 @@
 angular.module('livroApp').controller('PrincipalController', ['$scope', 'LivroService', function ($scope, LivroService) {
   $scope.livros = LivroService.query();
+  console.log('111');
 }]);
